b061248791b842c682b8a34bfcf1a34f0aa769cd
SRINJOY DAS <srinjoydas1104@gmail.com>
2024-12-17 19:27:39 +0530

    very simple chat app created but only displays the current message

--- frontend-ws/src/App.tsx
+++ frontend-ws/src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [socket , setSocket] = useState<null | WebSocket>(null);
+  const [latestMessage , setlatestMessage] = useState("");
+  const [InputMessage , setInputMessage] = useState("");
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:8081'); //connection establish with the web socker server
+    socket.onopen = () => { //when the socket connetion is open set the state to that socket conn and log connected
+      setSocket(socket); //set the status of the ws conn to connected that is opened
+      console.log("connected!!");
+    }
+    //the thread reaches reaches here whenever there is a message being sent on the ws server 
+    socket.onmessage = (message) => { //whenver there is a message on the socket connection then display the message 
+      console.log("received message : ", message.data);
+      setlatestMessage(message.data); //set the status to whatever the latest message that came from the server 
+    }
+  },[]);
+
+  if(!socket){ //when ever there is no socket conn established it shows this message 
+    return <div>
+      Connecting to Socket Server Loading ...
+    </div>
+  }
 
   return (
-    <>
+    <div>
+
+      <input onChange={((e) => {
+        setInputMessage(e.target.value);
+      })}></input>
+
+      <button onClick={() => {
+        socket.send(InputMessage)
+      }}>Send</button>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {latestMessage}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
 
-export default App
+export default App;
